e636059bcbf1c2dbf35a4c70ef18a39d40f4db70
gatty1 <gbolagadeajart4192@gmail.com>
2024-10-31 11:58:37 +0100

    new update

---
+++ predicted.py
@@ -0,0 +1,13 @@
+import joblib
+import numpy
+
+def prediction(Cement, BFS, Fly_Ash, Water, Superplasticizer, Coarse_Aggregate, Fine_Aggregate, Age):
+    
+    # Load GradientBoost Regression Model
+    model = joblib.load("concrete_compressive_model_GB.sav")
+    
+           
+    # Making Prediction
+    result = model.predict([[Cement, BFS, Fly_Ash, Water, Superplasticizer, Coarse_Aggregate, Fine_Aggregate, Age]])
+    result = numpy.round(result, 4)
+    return result
